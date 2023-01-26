64d12ad1e3ecb7731accee1d4c4fd04475af66e9
Himeth Sanjula <102307910+HimethSanjula@users.noreply.github.com>
2023-01-26 09:17:18 +0530

    for updating the new version of shoplaza

--- Product_Function.test.js
+++
@@ -1,5 +0,0 @@
-const Product_Function = require('./Product_Function');
-
-test(' When use purchase products from store. System will display available product count.  In this unit test Product count is 20, Sell count is 10. Therefore, available count is 10', () => {
-  expect(Product_Function(20, 10)).toBe(10);
-});
