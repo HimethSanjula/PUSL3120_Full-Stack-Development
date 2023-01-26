95001ed289846a59d8b209d0ba3affaf070f19f2
Himeth Sanjula <102307910+HimethSanjula@users.noreply.github.com>
2023-01-26 09:16:45 +0530

    for updating the new version of shoplaza

--- shoplaza-frontend/src/components/shared/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Input } from './input/Input';
-export { default as Loading } from './loading/Loading';
-export { default as Alert } from './alert/Alert';
-export { default as CustomSelect } from './custom-select/CustomSelect';
--- shoplaza-frontend/src/pages/admin/index.js
+++
@@ -1,4 +0,0 @@
-// export admin pages
-
-// export admin-dashbaord page from admin-dashboard folder
-export { default as AdminDashboard } from './admin-dashboard/admin-dashboard';
